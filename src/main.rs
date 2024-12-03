9ed092b724dcd1d6601feca8bc80b42d7d241b87
winarama <winarama@gmail.com>
2024-12-03 11:36:24 +0000

    day 3 solution

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,35 @@
 pub const GOOD_WEIGHT: f32 = 1.0;
 pub const BAD_WEIGHT: f32 = 2.0;
 
+fn main() {
+    println!("Little Timmy is nice >> {}", is_nice(10, 1));
+}
+
 pub fn is_nice(good_deeds: u32, bad_deeds: u32) -> bool {
-    // Calculate the ratio of good deeds to total deeds
-    // Any ratio greater than 0.75 is considered nice
-    // e.g. 10 good deeds and 2 bad deeds =
-    // (10 * 1) / (10 * 1) + (2 * 2) = 10 / 14 = 0.714... (not nice)
-    // If both good and bad deeds are 0, the child is naughty
+    let gdw = good_deeds as f64 * GOOD_WEIGHT as f64;
+    let bdw = bad_deeds as f64 * BAD_WEIGHT as f64;
+
+    let ratio = gdw / (gdw + bdw);
+    if ratio >= 0.75 {
+        return true
+    } else {
+        return false
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_nice() {
+        let result_true = super::is_nice(10, 1);
+        assert_eq!(result_true, true);
+
+        let result_false = super::is_nice(4, 10);
+        assert_eq!(result_false, false);
+
+        let result_default = super::is_nice(0, 0);
+        assert_eq!(result_default, false);
+    }
 }
