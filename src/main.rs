4347f56e18f770bbe307ed90e7d8651886823541
winarama <winarama@gmail.com>
2024-12-03 09:36:30 +0000

    day 3 problem

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-pub fn main() {
-    let gift_message = String::from("Merry Christmas! Enjoy your gift!");
-    attach_message_to_present(&gift_message);
+// We need to find the nice and naughty kids for santa
 
-    println!("{}", gift_message);
-}
+// Each good deed is worth 1 point and each bad deed is worth 2 points
+pub const GOOD_WEIGHT: f32 = 1.0;
+pub const BAD_WEIGHT: f32 = 2.0;
 
-pub fn attach_message_to_present(message: &str) {
-    println!("The present now has this message: {}", message);
+pub fn is_nice(good_deeds: u32, bad_deeds: u32) -> bool {
+    // Calculate the ratio of good deeds to total deeds
+    // Any ratio greater than 0.75 is considered nice
+    // e.g. 10 good deeds and 2 bad deeds =
+    // (10 * 1) / (10 * 1) + (2 * 2) = 10 / 14 = 0.714... (not nice)
+    // If both good and bad deeds are 0, the child is naughty
 }
