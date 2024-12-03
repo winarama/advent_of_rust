543ae461a2802f5bf9b1e848c561fbffb00dcd5b
winarama <winarama@gmail.com>
2024-12-03 09:23:01 +0000

    day 1 solution

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 pub fn main() {
     let gift_message = String::from("Merry Christmas! Enjoy your gift!");
-    attach_message_to_present(gift_message);
+    attach_message_to_present(gift_message.clone());
 
     println!("{}", gift_message);
 }
